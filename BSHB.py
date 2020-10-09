54948377cb0228f16b3a63599b22f4dadd54b07a
rahat-vau <71525581+rahat-vau@users.noreply.github.com>
2020-10-10 02:31:43 +0600

    Update BSHB.py

--- BSHB.py
+++ BSHB.py
@@ -1,10 +1,29 @@
+import sys
 import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
-prit("Hello guys")
-print("Assalamu Alaikum")
-print("I am")
+###############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+###############
+
+os.system("clear")
+
+
+
+
 os.system("figlet RAHAT")
-print("from Bangladesh Spammer & Hacker Boys")
+
 os.system("figlet JOY DA") CEO
 os.system("figlet MAMUN VAI") SENIOR ADMIN
 os.system("figlet RAJ VAI") ALL BOX LEADER
